6f9f4d68562897a134118e76a7c6cc0fe7dfca57
pratikshaHattikatti <52101504+pratikshaHattikatti@users.noreply.github.com>
2024-08-12 18:58:36 +0530

    Update ColorPair.java

--- colorcoder/ColorPair.java
+++ colorcoder/ColorPair.java
@@ -50,23 +50,6 @@ public class ColorPair {
         return major.getIndex() * numberOfMinorColors + minor.getIndex() + 1;
     }
     
-    static void testNumberToPair(int pairNumber,
-            MajorColor expectedMajor,
-            MinorColor expectedMinor)
-        {
-            ColorPair colorPair = GetColorFromPairNumber(pairNumber);
-            System.out.println("Got pair " + colorPair.ToString());
-            assert(colorPair.getMajor() == expectedMajor);
-            assert(colorPair.getMinor() == expectedMinor);
-        }
 
-        static void testPairToNumber(
-            MajorColor major,
-            MinorColor minor,
-            int expectedPairNumber)
-        {
-            int pairNumber = GetPairNumberFromColor(major, minor);
-            System.out.println("Got pair number " + pairNumber);
-            assert(pairNumber == expectedPairNumber);
-        }
+
 }
